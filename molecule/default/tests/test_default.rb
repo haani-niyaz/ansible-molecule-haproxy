fc29d4bbb684b9c8d1998495a6044a5e65b6245b
Haani Niyaz <haani.niyaz@gmail.com>
2018-06-19 16:14:47 +1000

    Molecule default basic scenario

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -5,3 +5,20 @@ describe file('/etc/hosts') do
   its('group') { should eq 'root' }
   its('mode') { should cmp '0644' }
 end
+
+describe docker_container(name: 'haproxy') do
+  it { should exist }
+  it { should be_running }
+  its('ports') { should eq '0.0.0.0:80->80/tcp' }
+end
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its('mode') { should cmp '0644' }
+end
+
+describe http('http://localhost:80') do
+  its('status') { should cmp 200 }
+end
